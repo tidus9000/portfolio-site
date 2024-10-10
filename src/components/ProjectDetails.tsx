66cb1621ad8967c34ff770cb05658317c27f8901
Arthur Muddiman <arthur@condensereality.com>
2024-10-10 15:10:59 +0100

    Can now select different projects in portfolio

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -10,8 +10,26 @@ interface Props {
   back: () => void;
 }
 
-const ProjectDetails = () => {
-  return <div>ProjectDetails</div>;
+const ProjectDetails = ({
+  name,
+  description,
+  technologies,
+  githubUrl,
+  heroimage,
+  markdown,
+  back,
+}: Props) => {
+  return (
+    <div>
+      <img src={heroimage} alt={name} />
+      <div>{name}</div>
+      <div>{description}</div>
+      <div>{technologies.join(", ")}</div>
+      <div>{githubUrl}</div>
+      <div>{markdown}</div>
+      <button onClick={back}>Back</button>
+    </div>
+  );
 };
 
 export default ProjectDetails;
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 import Card from "../components/Card";
+import ProjectDetails from "../components/ProjectDetails";
 import projects from "../assets/portfolio.json";
 
 const Portfolio = () => {
@@ -9,15 +10,26 @@ const Portfolio = () => {
 
   return (
     <div>
-      {projects.map((project, index) => (
-        <Card
-          key={index}
-          name={project.name}
-          description={project.description}
-          imageUrl={project.image}
-          onSelectItem={() => console.log("Clicked", index)}
+      {activeIndex === -1 &&
+        projects.map((project, index) => (
+          <Card
+            key={index}
+            name={project.name}
+            description={project.description}
+            imageUrl={project.image}
+            onSelectItem={() => setActiveIndex(index)}
+          />
+        ))}
+      {activeIndex !== -1 && (
+        <ProjectDetails
+          name={projects[activeIndex].name}
+          description={projects[activeIndex].description}
+          technologies={projects[activeIndex].technologies}
+          heroimage={projects[activeIndex].image}
+          markdown={projects[activeIndex].markdown}
+          back={() => setActiveIndex(-1)}
         />
-      ))}
+      )}
     </div>
   );
 };
